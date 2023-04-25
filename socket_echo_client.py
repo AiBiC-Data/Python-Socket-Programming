cec1dae4ea04ad2b20789d5c952418c79518e2d2
AiBiC-Data <whrnjsgh12@naver.com>
2023-04-25 22:15:02 +0900

    put statement

--- socket_echo_client.py
+++ socket_echo_client.py
@@ -4,16 +4,16 @@
 # 영어, 키보드 상에 존재하는 글자는 모두 1바이트
 # 한글은 euc-kr 한 글자당 2바이트. ex) 대한민국 만세->13bytes
 # utf-8 한 글자당 3바이트. ex) 대한민국 만세-> 19bytes
-import socket
+import socket        # 소켓 패키지 임포트
 
-sock = socket.socket()
-sock.connect(('127.0.0.1', 4400))
-while True:
-    print('Input: (exit:Exit)', end=' ')
-    user_input = input()
-    sock.send(user_input.encode('utf-8'))
-    if user_input == 'EXIT':
-        sock.close()
-        break
-    resp = sock.recv(1024)
-    print(resp.decode('utf-8'))
+sock = socket.socket()           # 소켓 생성
+sock.connect(('127.0.0.1', 4400))      # 로컬 호스트에 4400포트로 연결
+while True:                            # 통신을 반복
+    print('Input: (exit:EXIT)', end=' ')       # 입력 문구 출력
+    user_input = input()       # input 입력
+    sock.send(user_input.encode('utf-8'))           # 입력한 인풋을 인코딩하여 소켓 서버로 보내줌
+    if user_input == 'EXIT':        # 입력으로 EXIT이 들어오면 연결 종료
+        sock.close()             # 소켓 종료
+        break            # 반복문 종료
+    resp = sock.recv(1024)      # 소켓 서버로부터 받은 데이터를 resp에 저장
+    print(resp.decode('utf-8'))          # resp를 디코딩하여 출력
--- socket_echo_server.py
+++ socket_echo_server.py
@@ -3,50 +3,56 @@
 # listen -> 소켓이 들어오는 것을 기다린다.
 # accpet -> 실행되려면 bind되어 있어야 하고 listen상태여야한다.
 # close -> 소켓 서버나 소켓 클라이언트가 사용되지 않을 때, 만들어진 자원을 운영체제로 반환
-import socket
-import collections
+import socket        # 소켓 패키지 임포트
+import collections      # defaultdict를 이용하기 위한 collections 패키지 임포트
 
+# storage 딕셔너리 생성, 모든 키에 대해서 값이 없는 경우 자동으로 생성자의 인자로 넘어온 함수를 호출하여 그 결과값으로 설정
 storage = collections.defaultdict(str)
-sock = socket.socket()
+sock = socket.socket()          # 소켓 생성
+# 서버가 로컬 호스트의 모든 ip로의 연결에 대해 소켓을 네트워크 자원인 4400 포트에 매핑
 sock.bind(('0.0.0.0', 4400))
-sock.listen()
+sock.listen()  # 클라이언트가 bind된 포트로 연결을 할 때까지 기다리는 블럭킹 함수
 
-# res = (conn, address)로 구성
-# conn = 클라이언트와의 연결을 위한 새로운 소켓 객체
-# address = (host, port)로 구성
-# while True:
+# conn = 클라이언트와의 연결을 위한 새로운 소켓 객체로 데이터를 주고 받을 수 있는 창구, address = (host, port)로 구성
 conn, address = sock.accept()
-while True:
-    data = conn.recv(1024)
+while True:              # 통신을 지속
+    data = conn.recv(1024)                       # 클라이언트에서 보낸 데이터 받아 data에 저장
+    # 받은 데이터가 없거나 EXIT이면 연결을 종료
     if not data or data.decode('utf-8') == 'EXIT':
-        conn.close()
-        break
+        conn.close()                # 연결 종료
+        break             # 반복문 종료
+    # 받은 데이터를 콤마(,)를 기준으로 split하여 data_split에 리스트로 저장
     data_split = data.decode('utf-8').split(',')
-    print(data_split)
-    if data_split[0] == 'PUT':
-        if len(data_split) == 3:
+    if data_split[0] == 'PUT':            # 사용자가 PUT을 입력했을 때
+        if len(data_split) == 3:                 # PUT을 입력할 때 PUT,textA,textB로 입력하였는지 체크
+            # storage 딕셔너리에 textA:textB 형태로 저장
             storage[data_split[1]] = data_split[2]
+            # 클라이언트에게 잘 저장되었다고 알려줌
             conn.send('Success!'.encode('utf-8'))
-        else:
-            conn.send("Input Error!".encode('utf-8'))
+        else:                                       # PUT구문의 입력이 이상할 때
+            conn.send("Input Error!".encode('utf-8'))            # input에러 송신
+    # 사용자가 GET이나 DELETE 입력했을 때
     elif data_split[0] == 'GET' or data_split[0] == 'DELETE':
-        if len(data_split) != 2:
-            conn.send("Input Error!".encode('utf-8'))
-        elif data_split[1] not in storage:
-            conn.send("Not exist!".encode('utf-8'))
-        else:
-            if data_split[0] == 'GET':
-                conn.send(storage[data_split[1]].encode('utf-8'))
-            else:
+        if len(data_split) != 2:                    # 구문 입력이 이상할 때
+            conn.send("Input Error!".encode('utf-8'))           # input에러 송신
+        elif data_split[1] not in storage:              # 찾거나 삭제하려는 키가 딕셔너리에 없을 때
+            conn.send("Not exist!".encode('utf-8'))          # Not exist 송신
+        else:                        # GET과 DELETE 구분
+            if data_split[0] == 'GET':                          # GET이 입력일 때
+                # textA에 해당하는 value 값 송신
+                conn.sendall(storage[data_split[1]].encode('utf-8'))
+            else:                        # DELETE가 입력일 때
+                # textA에 해당하는 ket:value쌍을 딕셔너리에서 제거
                 del storage[data_split[1]]
-                conn.send('Success!'.encode('utf-8'))
-    elif data_split[0] == 'LIST':
-        if storage:
-            s = ''
-            for k, v in storage.items():
-                s += f'{k},{v}\n'
-            conn.send(s[:-1].encode('utf-8'))
-        else:
-            conn.send("Not exist!".encode('utf-8'))
-    else:
-        conn.send('ERROR!'.encode('utf-8'))
+                conn.send('Success!'.encode('utf-8'))      # 클라이언트에게 제거 알림
+    elif data_split[0] == 'LIST':            # LIST가 인풋일 때
+        if storage:                 # 빈 딕셔너리가 아니라면
+            s = ''                    # key,value 쌍을 저장할 빈 문자열 생성
+            for k, v in storage.items():       # 딕셔너리의 key:value쌍 반복
+                s += f'{k},{v}\n'            # 문자열 s에 해당값
+            # 값이 많을 수 있으므롤 sendall 이용
+            conn.sendall(s[:-1].encode('utf-8'))
+        else:                                     # 빈 딕셔너리일 경우
+            conn.send("Not exist!".encode('utf-8'))        # not exist 송신
+    else:                                 # 입력 구문이 잘 못 되었을 경우
+        conn.send('ERROR!'.encode('utf-8'))            # ERROR 송신
