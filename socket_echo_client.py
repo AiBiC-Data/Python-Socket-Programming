bf6fa5a7990f83d35804d8cf4862c584ff13974a
AiBiC-Data <whrnjsgh12@naver.com>
2023-04-25 14:53:49 +0900

    socket programming specific

--- socket_echo_client.py
+++ socket_echo_client.py
@@ -1,7 +1,19 @@
+# connect는 소켓 클라이언트에서 사용
+# connect -> 소켓 서버에 접속
+# send -> 데이터를 보냄
+# 영어, 키보드 상에 존재하는 글자는 모두 1바이트
+# 한글은 euc-kr 한 글자당 2바이트. ex) 대한민국 만세->13bytes
+# utf-8 한 글자당 3바이트. ex) 대한민국 만세-> 19bytes
 import socket
 
-client = socket.socket()
-client.connect(('127.0.0.1', 6543))  # 서버와 접속
-# 데이터 전송, 바이트 형식으로 전송해야됨-> encode('utf-8')사용
-client.send('Hello Pyhton Network'.encode('utf-8'))
-print(client.recv(1024))
+sock = socket.socket()
+sock.connect(('127.0.0.1', 4400))
+while True:
+    print('Input: (exit:Exit)', end=' ')
+    user_input = input()
+    sock.send(user_input.encode('utf-8'))
+    if user_input == 'EXIT':
+        sock.close()
+        break
+    resp = sock.recv(1024)
+    print(resp.decode('utf-8'))
--- socket_echo_server.py
+++ socket_echo_server.py
@@ -1,11 +1,52 @@
+# bind, listen, accept는 소켓 서버에서 사용
+# bind -> 소켓을 주소패밀리에 묶어 준다.
+# listen -> 소켓이 들어오는 것을 기다린다.
+# accpet -> 실행되려면 bind되어 있어야 하고 listen상태여야한다.
+# close -> 소켓 서버나 소켓 클라이언트가 사용되지 않을 때, 만들어진 자원을 운영체제로 반환
 import socket
+import collections
 
-server = socket.socket()
-# 첫번째 인자를 받아야함->튜플형태로 생성 / 127.0.0.1은 내 컴퓨터에서 들어오는 요청만 연결하겠다, 0.0.0.0은 모든 네트워크 연결허용하겠다.
-server.bind(('0.0.0.0', 6543))
-server.listen()
+storage = collections.defaultdict(str)
+sock = socket.socket()
+sock.bind(('0.0.0.0', 4400))
+sock.listen()
 
+# res = (conn, address)로 구성
+# conn = 클라이언트와의 연결을 위한 새로운 소켓 객체
+# address = (host, port)로 구성
+# while True:
+conn, address = sock.accept()
 while True:
-    conn, addr = server.accept()  # 데이터 받기
-    data = conn.recv(1024)  # 받은 데이터 저장
-    conn.send(data)  # 받은 데이터 다시 전송
+    data = conn.recv(1024)
+    if not data or data.decode('utf-8') == 'EXIT':
+        conn.close()
+        break
+    data_split = data.decode('utf-8').split(',')
+    print(data_split)
+    if data_split[0] == 'PUT':
+        if len(data_split) == 3:
+            storage[data_split[1]] = data_split[2]
+            conn.send('Success!'.encode('utf-8'))
+        else:
+            conn.send("Input Error!".encode('utf-8'))
+    elif data_split[0] == 'GET' or data_split[0] == 'DELETE':
+        if len(data_split) != 2:
+            conn.send("Input Error!".encode('utf-8'))
+        elif data_split[1] not in storage:
+            conn.send("Not exist!".encode('utf-8'))
+        else:
+            if data_split[0] == 'GET':
+                conn.send(storage[data_split[1]].encode('utf-8'))
+            else:
+                del storage[data_split[1]]
+                conn.send('Success!'.encode('utf-8'))
+    elif data_split[0] == 'LIST':
+        if storage:
+            s = ''
+            for k, v in storage.items():
+                s += f'{k},{v}\n'
+            conn.send(s[:-1].encode('utf-8'))
+        else:
+            conn.send("Not exist!".encode('utf-8'))
+    else:
+        conn.send('ERROR!'.encode('utf-8'))
